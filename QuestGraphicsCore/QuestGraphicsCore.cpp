d95b7700d099fa1e825107a1ddb90ba96d97e8ae
Paul Burgess <paulburgess1357@gmail.com>
2021-11-02 20:43:51 -0600

    solution restructure

---
+++ QuestGraphicsCore/QuestGraphicsCore.cpp
@@ -0,0 +1,10 @@
+// QuestGraphicsCore.cpp : Defines the functions for the static library.
+//
+
+#include "pch.h"
+#include "framework.h"
+
+// TODO: This is an example of a library function
+void fnQuestGraphicsCore()
+{
+}
--- QuestGraphicsCore/pch.h
+++ QuestGraphicsCore/pch.h
@@ -9,7 +9,5 @@
 
 // add headers that you want to pre-compile here
 #include "framework.h"
-#include "Graphics/Bind.h"
-#include "Graphics/Handle.h"
 
 #endif //PCH_H
