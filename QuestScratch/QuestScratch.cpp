271411d590fe2bdfbb1437b3d17b9ea4ecdb3164
Paul Burgess <paulburgess1357@gmail.com>
2021-11-06 14:36:50 -0600

    in progress

--- QuestScratch/QuestScratch.cpp
+++ QuestScratch/QuestScratch.cpp
@@ -7,7 +7,6 @@ int main(){
     QuestWindow::Window window{ 1920, 1080, 3, 3};
 
     while (!window.close_window()){
-        
         glfwPollEvents();
         window.clear_buffer();
         window.swap_buffer();
