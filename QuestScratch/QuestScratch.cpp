9f52e593ca42e54917ca4778cdc854d84a69596a
Paul Burgess <paulburgess1357@gmail.com>
2021-11-15 20:46:28 -0700

    Working keyboard input

--- QuestScratch/QuestScratch.cpp
+++ QuestScratch/QuestScratch.cpp
@@ -2,11 +2,9 @@
 
 int main(){
 
-    int x = 500;
+    const QuestWindow::Window window{ 1920, 1080, 3, 3};
 
-    QuestWindow::Window window{ 1920, 1080, 3, 3};
-
-    while (!window.close_window()){
+    while (!window.close_window() && !QuestWindow::KeyboardInput::is_pressed(QuestWindow::Keyboard::ESCAPE)){
         glfwPollEvents();
         window.clear_buffer();
         window.swap_buffer();
